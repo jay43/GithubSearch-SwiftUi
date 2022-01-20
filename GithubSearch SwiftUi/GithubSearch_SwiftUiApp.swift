07011f15f55351d4f84e5c67e41e6779535df331
Prayag Gediya <prayaggediya60@gmail.com>
2022-01-20 10:48:08 +0530

    Initial Commit

---
+++ GithubSearch SwiftUi/GithubSearch_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearch_SwiftUiApp.swift
+//  GithubSearch SwiftUi
+//
+//  Created by PC on 20/01/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearch_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
