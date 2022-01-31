d58d95d2526265d7b435e9fabe6ec32d897b902c
Jay Khunt <jaykhunt741@gmail.com>
2022-01-31 10:22:27 +0530

    Introducing Container views

--- GithubSearch SwiftUi/ContentView.swift
+++ GithubSearch SwiftUi/ContentView.swift
@@ -8,22 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var query: String = "java"
-    @EnvironmentObject var repoStore: ReposStore
     
     var body: some View {
         NavigationView {
-            List {
-                TextField("Type something...", text: $query, onCommit: fetch)
-                ForEach(repoStore.repos) { repo in
-                    RepoRow(repo: repo)
-                }
-            }.navigationBarTitle(Text("Search"))
-        }.onAppear(perform: fetch)
-    }
-    
-    private func fetch() {
-        repoStore.fetch(matching: query)
+            SearchContainerView()
+                .navigationBarTitle(Text("Search"))
+        }
     }
 }
 
@@ -32,3 +22,44 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//Container Views should do things only related to data-flow:
+//
+//1. Store the state of the Rendering View
+//2. Fetch data using ObservableObject
+//3. Handle life cycle (onAppear/onDisappear)
+//4. Provide action handlers to the Rendering View
+struct SearchContainerView: View {
+    @EnvironmentObject var repoStore: ReposStore
+    @State var query = "Swift"
+    
+    var body: some View {
+        SearchView(query: $query, repos: repoStore.repos, onCommit: fetch)
+            .onAppear(perform: fetch)
+    }
+    
+    private func fetch() {
+        repoStore.fetch(matching: query)
+    }
+}
+
+//Rendering Views should do things only related to rendering:
+//
+//1. Build User Interface using primitive components provided by SwiftUI.
+//2. Compose User Interface by using other Rendering Views.
+//3. Use data as input to render User Interface and don’t store any state.
+struct SearchView: View {
+    @Binding var query: String
+    let repos: [Repo]
+    let onCommit: ()-> Void
+    
+    var body: some View {
+        List {
+            TextField("Type something...", text: $query, onCommit: onCommit)
+            ForEach(repos) { repo in
+                RepoRow(repo: repo)
+            }
+        }
+    }
+}
+
